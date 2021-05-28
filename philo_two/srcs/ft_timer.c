5282dd03805de979dcaac5649eb4fbc2120c8adb
Mohamed El omary <mel-omar@e1r3p10.1337.ma>
2021-05-28 21:58:40 +0100

    to home

--- philo_two/srcs/ft_timer.c
+++ philo_two/srcs/ft_timer.c
@@ -6,7 +6,7 @@
 /*   By: mel-omar <mel-omar@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/29 21:34:41 by mel-omar          #+#    #+#             */
-/*   Updated: 2021/05/28 21:58:13 by mel-omar         ###   ########.fr       */
+/*   Updated: 2021/05/28 21:58:15 by mel-omar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,5 @@ long long	get_time(void)
 	struct timeval			time;
 
 	gettimeofday(&time, NULL);
-	return (time.tv_sec * 1000000 + time.tv_usec);
+	return (time.tv_sec * 1000 + time.tv_usec / 1000);
 }
-
-long long convert(long long micors)
-{
-	return (micors / 1000);
-}
-
-
